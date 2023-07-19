c6284892e8eddfab751087c8896c1636cfdf4cd8
AgustinItuarte <agustin242536@gmail.com>
2023-07-18 22:07:08 -0300

    Estructure of the page added

--- memory-card/src/App.js
+++ memory-card/src/App.js
@@ -1,6 +1,11 @@
 function App() {
   return (
     <div className="App">
+      <div className="header">
+        <div className="title">Title</div>
+        <div className="score">Score</div>
+      </div>
+      <div className="card-list"></div>
     </div>
   );
 }
