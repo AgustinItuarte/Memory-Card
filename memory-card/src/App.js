ac87fce2cb8ec187f999299ce9ea6d43c37d1a68
AgustinItuarte <agustin242536@gmail.com>
2023-07-23 19:24:24 -0300

    Img Added

--- memory-card/src/App.js
+++ memory-card/src/App.js
@@ -1,32 +1,45 @@
 import React, {useState} from 'react';
 import uniqid from 'uniqid';
+import LoadCards from './components/Cards';
+import image1 from './assets/Ajantis.webp';
+import image2 from './assets/Coran.webp';
+import image3 from './assets/Edwin.webp';
+import image4 from './assets/Eldoth.webp';
+import image5 from './assets/Garrick.webp';
+import image6 from './assets/Kagain.webp';
+import image7 from './assets/Khalid.webp';
+import image8 from './assets/Kivan.webp';
+import image9 from './assets/Minsc.webp';
+import image10 from './assets/Montaron.webp';
+import image11 from './assets/Quayle.webp';
+import image12 from './assets/Tiax.webp';
 
 function App() { 
 
   const [cards, setCards] = useState([
-    {src: '',
+    {src: image1,
      id: uniqid() },
-    {src: '',
+    {src: image2,
     id: uniqid() },
-    {src: '',
+    {src: image3,
     id: uniqid() },
-    {src: '',
+    {src: image4,
     id: uniqid() },
-    {src: '',
+    {src: image5,
     id: uniqid() },
-    {src: '',
+    {src: image6,
     id: uniqid() },
-    {src: '',
+    {src: image7,
     id: uniqid() },
-    {src: '',
+    {src: image8,
     id: uniqid() },
-    {src: '',
+    {src: image9,
     id: uniqid() },
-    {src: '',
+    {src: image10,
     id: uniqid() },
-    {src: '',
+    {src: image11,
     id: uniqid() },
-    {src: '',
+    {src: image12,
     id: uniqid() },
   ]
   )
@@ -37,7 +50,11 @@ function App() {
         <div className="title">Title</div>
         <div className="score">Score</div>
       </div>
-      <div className="card-list"></div>
+      <div className="card-list">
+        <div className='cards'>
+          <LoadCards cardsArray={cards}></LoadCards>
+        </div>
+      </div>
     </div>
   );
 }
--- memory-card/src/components/Cards.js
+++ memory-card/src/components/Cards.js
@@ -1,3 +1,13 @@
 function LoadCards(props) {
-    
+
+    return props.cardsArray.map(card => {
+        console.log(card.id)
+        return(
+            <img src={card.src} data-id={card.id} style={{width:'100%', height:'100%'}}></img>          
+        )
+
+    });
+
 }
+
+export default LoadCards;
